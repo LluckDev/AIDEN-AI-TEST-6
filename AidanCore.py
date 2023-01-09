119fce4d91fe7de600569b76ef13b55333f9cec5
henry <henryshoffacker@gmail.com>
2023-01-09 02:29:44 -0600

    made the colors for the GUI

--- AidanCore.py
+++ AidanCore.py
@@ -59,8 +59,23 @@ class AidanCore:
         #creates window
         self.w = display(screenX=800,screenY=600,title="AIDEN Ai",)
         self.w.sendkeys(self.keys)
-        self.w.addcolorpalette("red","#ff0000")
-        self.w.rect("test",50,50,20,20,fill=self.w.cp["red"])
+
+
+
+
+
+
+        #color palette
+
+
+        self.w.addcolorpalette("BG dark","#474747")
+        self.w.addcolorpalette("BG mid", "#575757")
+        self.w.addcolorpalette("BG light", "#676767")
+        self.w.addcolorpalette("BG bright", "#7D7D7D")
+        self.w.addcolorpalette("text","#E0E0E0")
+        self.w.addcolorpalette("hl","#4D8EF0")
+
+
 
 
 
